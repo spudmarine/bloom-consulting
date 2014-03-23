eeb970a948f834f347c7d70ce435180bd1ffd97e
Scott Rowland <scrowlan@gmail.com>
2014-03-22 17:33:08 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BloomConsulting::Application.config.secret_key_base = '0e13e7db431f3e37b755582c373f1a95edb278b79673b53499f3cf37510543affa66ea28567d796b8306e47f6b9bfbace66cf6f6aaa44eacf7530b4aa2cb5a11'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BloomConsulting::Application.config.session_store :cookie_store, key: '_bloom-consulting_session'
