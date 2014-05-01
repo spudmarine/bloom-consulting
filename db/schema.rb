5925c0ff8ada34cffab8bc81d1e3a6a435a04bc9
Jenny <jenmjayne@gmail.com>
2014-05-01 14:22:15 -0500

    JJ: make social links adminable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501025520) do
+ActiveRecord::Schema.define(version: 20140501185506) do
 
   create_table "abouts", force: true do |t|
     t.string   "hero_image"
@@ -23,35 +23,6 @@ ActiveRecord::Schema.define(version: 20140501025520) do
     t.text     "area2_copy"
     t.string   "area3_title"
     t.text     "area3_copy"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "admin_mains", force: true do |t|
-    t.string   "hero_image"
-    t.string   "hero_title"
-    t.string   "stratum_1_image"
-    t.string   "stratum_1_title"
-    t.text     "stratum_1_copy"
-    t.string   "stratum_1_cta"
-    t.string   "stratum_1_link"
-    t.string   "practice_area_1_image"
-    t.string   "practice_area_1_title"
-    t.text     "practice_area_1_copy"
-    t.string   "practice_area_1_link"
-    t.string   "practice_area_2_image"
-    t.string   "practice_area_2_title"
-    t.text     "practice_area_2_copy"
-    t.string   "practice_area_2_link"
-    t.string   "practice_area_3_image"
-    t.string   "practice_area_3_title"
-    t.text     "practice_area_3_copy"
-    t.string   "practice_area_3_link"
-    t.string   "stratum_2_image"
-    t.string   "stratum_2_title"
-    t.text     "stratum_2_copy"
-    t.string   "stratum_2_cta"
-    t.string   "stratum_2_link"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -84,6 +55,9 @@ ActiveRecord::Schema.define(version: 20140501025520) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "twitter"
+    t.string   "facebook"
+    t.string   "linkedin"
   end
 
   create_table "employees", force: true do |t|
