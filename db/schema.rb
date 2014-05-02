8b3b6215e34b50d6512a9252a289edea67fe62ba
Scott Rowland <scrowlan@gmail.com>
2014-05-02 16:57:56 -0500

    added section in BI for design elements

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140502025157) do
+ActiveRecord::Schema.define(version: 20140502213634) do
 
   create_table "abouts", force: true do |t|
     t.string   "hero_image"
@@ -198,6 +198,8 @@ ActiveRecord::Schema.define(version: 20140502025157) do
     t.text     "pm_knowledge_copy"
     t.string   "pm_deliverables_title"
     t.text     "pm_deliverables_copy"
+    t.string   "bifocus_title"
+    t.text     "bifocus_copy"
   end
 
   create_table "quotes", force: true do |t|
