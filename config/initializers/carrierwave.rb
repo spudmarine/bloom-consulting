9fe381bcea0d7bfc019a9d26d2bf2e757a8d55f7
Scott Rowland <scrowlan@gmail.com>
2014-05-05 14:18:55 -0500

    update AWS access keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,13 +4,13 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAJ6YD3FERGERODMSQ',                        # required
-    :aws_secret_access_key  => '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',                     # required
+    :aws_access_key_id      => 'AKIAJC4EFI7Y6YYTBQQA',                        # required
+    :aws_secret_access_key  => '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',                     # required
     :region                 => 'us-west-2',                  # optional, defaults to 'us-east-1'
     # :host                   => 's3.example.com',             # optional, defaults to nil
     # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = 'bloom-site-images'                             # required
+  config.fog_directory  = 'bcg-site-assets'                             # required
   config.fog_public     = false                                   # optional, defaults to true
   # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
  
