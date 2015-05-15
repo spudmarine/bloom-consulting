8914f136244fc096433f9c3c06a7ac281839ecd5
Scott Rowland <scrowlan@gmail.com>
2015-05-14 19:52:00 -0500

    update migration

--- db/migrate/20150513211002_add_p_as.rb
+++ db/migrate/20150513211002_add_p_as.rb
@@ -1,8 +1,5 @@
 class AddPAs < ActiveRecord::Migration
   def change
-  	remove_column :mains, :practice_area_4_image
-  	remove_column :mains, :practice_area_4_title
-  	remove_column :mains, :practice_area_4_copy
 
   	add_column :mains, :practice_area_4_image, :string
   	add_column :mains, :practice_area_4_title, :string
