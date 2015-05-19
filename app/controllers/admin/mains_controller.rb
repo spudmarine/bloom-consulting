627da215664c535e6cffe103baf97b1865dd5034
Scott Rowland <scrowlan@gmail.com>
2015-05-19 12:54:05 -0500

    updates for cleaning up old logic

--- app/controllers/admin/mains_controller.rb
+++ app/controllers/admin/mains_controller.rb
@@ -39,6 +39,6 @@ class Admin::MainsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def main_params
-      params.require(:main).permit(:hero_image, :hero_title, :stratum_1_image, :stratum_1_title, :stratum_1_copy, :stratum_1_cta, :stratum_1_link, :practice_area_1_image, :practice_area_1_title, :practice_area_1_copy, :practice_area_1_link, :practice_area_2_image, :practice_area_2_title, :practice_area_2_copy, :practice_area_2_link, :practice_area_3_image, :practice_area_3_title, :practice_area_3_copy, :practice_area_3_link, :stratum_2_image, :stratum_2_title, :stratum_2_copy, :stratum_2_cta, :stratum_2_link, :practice_area_4_image, :practice_area_4_title, :practice_area_4_copy, :practice_area_4_link)
+      params.require(:main).permit(:hero_image, :hero_title)
     end
 end
--- app/models/main.rb
+++ app/models/main.rb
@@ -1,12 +1,6 @@
 class Main < ActiveRecord::Base
 	
 	mount_uploader :hero_image, HeroImageUploader
-	mount_uploader :practice_area_1_image, PracticeAreaImageUploader
-	mount_uploader :practice_area_2_image, PracticeAreaImageUploader
-	mount_uploader :practice_area_3_image, PracticeAreaImageUploader
-	mount_uploader :practice_area_4_image, PracticeAreaImageUploader
-	mount_uploader :stratum_1_image, StratumImageUploader
-	mount_uploader :stratum_2_image, StratumImageUploader
 
-	validates :hero_title, :practice_area_1_image, :practice_area_1_title, :practice_area_1_copy, :practice_area_2_title, :practice_area_2_copy, :practice_area_3_title, :practice_area_3_copy, presence: true
+	validates :hero_title, presence: true
 end
